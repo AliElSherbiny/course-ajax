b3385cefede8099e1d0ddb647b1652cc6d6aa1b9
Ali El-Sherbiny <alielsherbiny142@gmail.com>
2021-06-05 10:27:55 +0200

    update file to fetch the first photo in api search results

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -1,4 +1,21 @@
-(function () {
+
+      const urlBase = 'https://api.unsplash.com/search/photos?page=1&query='
+      const urlKey = '&CqYaYzRkH_BhRDw-_mHBEatH3TsAy8Lb_ORZJG2Xly4'
+      const asyncRequestObject = new XMLHttpRequest();
+
+      function handleSuccess () { 
+        //console.log( this.responseText ); 
+        response = JSON.parse(this.responseText) ; 
+        responseContainer.innerHTML = `<img src=${response.results[0].urls.small} alt="Girl in a jacket" </img>`;
+        console.log( responseContainer.textContent); 
+      }
+      
+
+      function handleError () { 
+        console.log( 'An error occurred \uD83D\uDE1E' );
+      }
+
+
     const form = document.querySelector('#search-form');
     const searchField = document.querySelector('#search-keyword');
     let searchedForText;
@@ -8,5 +25,18 @@
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+         url = urlBase+searchedForText;
+         asyncRequestObject.open('GET', url);
+         asyncRequestObject.setRequestHeader('Authorization', 'Client-ID CqYaYzRkH_BhRDw-_mHBEatH3TsAy8Lb_ORZJG2Xly4');
+         asyncRequestObject.onload = handleSuccess;
+         asyncRequestObject.onerror = handleError;
+         asyncRequestObject.send();
     });
-})();
+
+
+
+
+
+
+ 
+
